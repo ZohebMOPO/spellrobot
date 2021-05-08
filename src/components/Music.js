6837cd477f880aad38382b36aaa5d3f589972acf
Zoheb Alli Khan <understandingzak@gmail.com>
2021-05-08 12:17:19 +0530

    Music Feature Added

--- src/components/Music.js
+++ src/components/Music.js
@@ -1,9 +1,21 @@
 import React from "react";
+import "./Music.css";
 import AudioPlayer from "react-audio-element";
 
 function Music() {
   return (
-    <AudioPlayer src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3" />
+    <div className="text-light">
+      <AudioPlayer
+        src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3"
+        overrideStyles={true}
+        classNames={{
+          controlButton: "custom-control",
+          playPause: "custom-play-pause",
+          timeText: "custom-time-text",
+          sliderTrack: "custom-slider-track",
+        }}
+      />
+    </div>
   );
 }
 
