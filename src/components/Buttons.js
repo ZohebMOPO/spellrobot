935d1c51773a037c64ac3d350bc3c21e55c343cf
Zoheb Alli Khan <understandingzak@gmail.com>
2021-05-09 09:40:50 +0530

    Bug Fixes

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,22 +1,27 @@
 import React, { Fragment } from "react";
 
 class Buttons extends React.Component {
+  state = { resp: Number };
   async componentDidMount() {
     const url = "https://tangled.ae/Spellbound-Robot/entry.php?val=1";
     const response = await fetch(url, {
-      mode: "cors",
+      mode: "no-cors",
       headers: {
         "Content-Type": "application/json",
       },
     });
     const data = response.json();
-    console.log(data);
   }
+  // eslint-disable-next-line no-dupe-class-members
   render() {
     return (
       <Fragment>
         <div className="text-center p-5">
-          <button type="button" class="btn-lg btn-secondary">
+          <button
+            type="button"
+            class="btn-lg btn-secondary"
+            onClick={() => this.state.data}
+          >
             Front
           </button>
         </div>
